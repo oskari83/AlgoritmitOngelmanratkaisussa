73ba8279e8889962045dd48a96f91370250c75fd
oskari83 <oskpelto@gmail.com>
2022-07-12 21:20:56 +0300

    14 done

--- TwoSetsII.cpp
+++ TwoSetsII.cpp
@@ -5,57 +5,34 @@
 using namespace std;
 typedef long long ll;
 
-set <vector <int> > options;
-
-void creator(vector <int> v, int len, int k, int curSum, int maxSum){
-    if(curSum==maxSum){
-        options.insert(v);
-    }
-    if(k>len || curSum>maxSum){
-        return;
-    }
-    int newK = k + 1;
-    int newSum = curSum + k;
-    for(int i=0;i<2;i++){
-        if(i==0){
-            v.push_back(k);
-            creator(v,len,newK,newSum,maxSum);
-            v.pop_back();
-        }else{
-            creator(v,len,newK,curSum,maxSum);
-        }
-    }
-}
-
 int main() {
     int n;
     cin >> n;
-    int ans = 0;
-    ll maxSum = (n*(n+1))/4;
-    vector <int> vec;
-    creator(vec,n,1,0,maxSum);
-    //cout << options.size() << "\n";
-    for(auto vec : options){
-        int n2 = vec.size();
-        int targetSum=0;
-        for(int i=0;i<n2;i++){
-            //cout << vec[i] << " ";
-            if(i==0){
-                if(vec[0]!=1){
-                    for(int j=1;j<vec[i];j++){
-                        targetSum+=j;
-                    }
-                }
-            }else{
-                for(int j=vec[i-1]+1;j<vec[i];j++){
-                    targetSum+=j;
+    ll sum = (n*(n+1))/2;
+    ll mod = 1e9+7;
+    if(sum%2==0){
+        ll t=sum/2;
+
+        int ways[sum+1];
+        ways[0]=1;
+        for(int i=1;i<=sum+1;i++){
+            ways[i]=0;
+        }
+
+        for(int k=1;k<=n;k++){
+            for(int x=t;x>=0;x--){
+                if(ways[x]>0){
+                    ways[x+k] += ways[x];
+                    ways[x+k] %= mod;
                 }
             }
         }
-        if(targetSum==maxSum){
-           ans+=1;
+        
+        if(ways[t]%2==1){
+            ways[t]+=mod;
         }
-        //cout << "\n";
+        cout << (ways[t]/2);
+    }else{
+        cout << 0;
     }
-    cout << ans;
 }
