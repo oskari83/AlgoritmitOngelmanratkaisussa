9b918736a68fd14180c3dfea5fb05fe2c9449010
oskari83 <oskpelto@gmail.com>
2022-09-03 10:31:22 +0300

    57 done

---
+++ 10 Strings/StringMatching.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <map>
+using namespace std;
+typedef long long ll;
+
+vector<int> z(string s) {
+    int n = s.size();
+    vector<int> z(n);
+    int x = 0, y = 0;
+    for (int i = 1; i < n; i++) {
+        z[i] = max(0,min(z[i-x],y-i+1));
+        while (i+z[i] < n && s[z[i]] == s[i+z[i]]) {
+            x = i; y = i+z[i]; z[i]++;
+        }
+    }
+    return z; 
+}
+
+int main(){
+    string st, pat;
+    cin >> st >> pat;
+    int m = pat.length();
+    string nst = pat + "#" + st;
+    //cout << "nst=" << nst;
+    vector<int> ztable = z(nst);
+    int ans = 0;
+    for(auto u : ztable){
+        //cout << u << "\n";
+        if(u==m){
+            ans++;
+        }
+    }
+
+    cout << ans;
+}
